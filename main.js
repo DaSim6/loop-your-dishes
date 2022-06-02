da17f5ef5080c591c02f0daf8be4538d9ddeac5f
DaSim6 <learner-9f5db4@extensionschool.ch>
2022-06-02 16:29:52 +0200

    LoopYourDishes Python 1st version

---
+++ main.js
@@ -0,0 +1,7 @@
+var i ;
+var favDishes = ["Tartare de saumon", "Gallo Pinto", "Souris d'Agneau"];
+console.log("Here are my favorite dishes: ");
+for(i= 0; i < favDishes.length; i++){
+    console.log(favDishes[i]);
+}
+console.log("What are yours ?");
